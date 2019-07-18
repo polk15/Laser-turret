2a87405ea1b64b5bffe4d7436052387a44bf2e98
husovschi <husovschi@protonmail.com>
2019-07-18 22:08:16 +0300

    Updated with non_max_suppression

--- test.py
+++ test.py
@@ -1,28 +1,25 @@
 import numpy as np
 import cv2
-
+from imutils.object_detection import non_max_suppression
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 cv2.startWindowThread()
 cap = cv2.VideoCapture(0)
 
-while (True):
+while True:
     ret, frame = cap.read()
     frame = cv2.resize(frame, (640, 480))
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    boxes, weights = hog.detectMultiScale(gray, winStride=(8, 8))
-    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-    coordinates = []
-    for (xA, yA, xB, yB) in boxes:
-        coordinates.append(xA)
-        coordinates.append(yA)
-        coordinates.append(xB)
-        coordinates.append(yB)
-        break
+    # gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    (rectangles, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8))
 
-    if len(coordinates) > 0:
-        cv2.rectangle(gray, (coordinates[0], coordinates[1]), (coordinates[2], coordinates[3]), (0, 255, 0), 5)
-    cv2.imshow('frame', gray)
+    rectangles = np.array([[x, y, x+width, y+height] for(x, y, width, height) in rectangles])
+    pick = non_max_suppression(rectangles, probs=None, overlapThresh=0.65)
+
+    for(xA, yA, xB, yB) in pick:
+        cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 5)
+
+    cv2.imshow('frame', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
